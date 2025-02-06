b6d4bafa74d1d361e994b35eb880b30652cc4480
cristian <cchura.cpc@gmail.com>
2025-02-05 22:22:20 -0400

    CRUD completo museo

--- app/Http/Controllers/MuseoController.php
+++ app/Http/Controllers/MuseoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Museo;
 use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\Return_;
+use Illuminate\Support\Facades\DB;
 
 class MuseoController extends Controller
 {
@@ -13,14 +13,19 @@ class MuseoController extends Controller
      */
     public function index(Request $request)
     {
-        $limit = isset($request->limit)?$request->limit:10;
+        try {
+            $limit = isset($request->limit) ? $request->limit : 10;
 
-        $museos = Museo::with(['categoria', 'comentarios', 'favoritos'])
-                        ->where("nombre", "like", "%$request->q%")
-                        ->orderBy('id', 'desc')
-                        ->paginate($limit);
+            $museos = Museo::with(['categoria', 'comentarios', 'favoritos'])
+                ->where("nombre", "like", "%$request->q%")
+                ->orderBy('id', 'desc')
+                ->paginate($limit);
 
-        return response()->json($museos, 200);
+            return response()->json($museos, 200);
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -28,26 +33,45 @@ class MuseoController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            "nombre" => "required",
-            "categoria_id" => "required"
-        ]);
+        try {
+            $request->validate([
+                "nombre" => "required|unique:museos",
+                "categoria_id" => "required"
+            ]);
 
-        $museo = new Museo();
-        $museo->nombre = $request->nombre;
-        $museo->descripcion = $request->descripcion;
-        $museo->horario_atencion = $request->horario_atencion;
-        $museo->direccion = $request->direccion;
-        $museo->latitud = $request->latitud;
-        $museo->longitud = $request->longitud;
-        if(isset($request->precio)){
-            $museo->precio = $request->precio;
+            DB::beginTransaction();
+
+            // subida de imagenes
+            $direccion_imagen = "";
+            if($file = $request->file("imagen")){
+                $url_imagen = time() ."-". $file->getClientOriginalName();
+                $file->move("imagenes/", $url_imagen);
+                $direccion_imagen = "imagenes/". $url_imagen;
+            }
+
+            $museo = new Museo();
+            $museo->nombre = $request->nombre;
+            $museo->descripcion = $request->descripcion;
+            $museo->horario_atencion = $request->horario_atencion;
+            $museo->direccion = $request->direccion;
+            $museo->latitud = $request->latitud;
+            $museo->longitud = $request->longitud;
+            if (isset($request->precio)) {
+                $museo->precio = $request->precio;
+            }
+            $museo->categoria_id = $request->categoria_id;
+            $museo->user_id = $request->user()->id;
+            $museo->imagen = $direccion_imagen;
+            $museo->save();
+
+            DB::commit();
+
+            return response()->json(["mensaje" => "Museo Registrado correctamente"], 201);
+        } catch (\Exception $e) {
+            //throw $th;
+            DB::rollback();
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
         }
-        $museo->categoria_id = $request->categoria_id;
-        $museo->user_id = $request->user()->id;
-        $museo->save();
-
-        return response()->json(["mensaje" => "Museo Registrado correctamente"], 201);
     }
 
     /**
@@ -55,7 +79,14 @@ class MuseoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $museo = Museo::with(['categoria', 'comentarios', 'favoritos'])->find($id);
+
+            return response()->json($museo, 200);
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -63,7 +94,50 @@ class MuseoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+
+                $request->validate([
+                    "nombre" => "required|unique:museos,nombre,$id",
+                    "categoria_id" => "required"
+                ]);
+    
+                DB::beginTransaction();
+    
+                // subida de imagenes
+                $direccion_imagen = "";
+                
+    
+                $museo = Museo::find($id);
+                $museo->nombre = $request->nombre;
+                $museo->descripcion = $request->descripcion;
+                $museo->horario_atencion = $request->horario_atencion;
+                $museo->direccion = $request->direccion;
+                $museo->latitud = $request->latitud;
+                $museo->longitud = $request->longitud;
+                if (isset($request->precio)) {
+                    $museo->precio = $request->precio;
+                }
+                $museo->categoria_id = $request->categoria_id;
+                $museo->user_id = $request->user()->id;
+
+                if($file = $request->file("imagen")){
+                    $url_imagen = time() ."-". $file->getClientOriginalName();
+                    $file->move("imagenes/", $url_imagen);
+                    $direccion_imagen = "imagenes/". $url_imagen;
+                    $museo->imagen = $direccion_imagen;
+                }
+
+                $museo->update();
+    
+                DB::commit();
+    
+                return response()->json(["mensaje" => "Museo Actualizado correctamente"], 201);
+    
+            
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -71,19 +145,33 @@ class MuseoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $museo = Museo::find($id);
+            // $museo->delete();
+            return response()->json(["mensaje" => "Museo Eliminado SIMULADO"], 200);
+
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
+        }
     }
 
-    public function funComentar($id, Request $request){
+    public function funComentar($id, Request $request)
+    {
+        try {
 
-        $user_id = $request->user()->id;
+            $user_id = $request->user()->id;
 
-        $museo = Museo::find($id);
-        $museo->comentarios()->attach([$user_id => [ 
-                                                    "comentario" => $request->comentario,
-                                                    "calificacion" => $request->calificacion,
-                                                    "fecha" => date("Y-m-d H:i:s") 
-                                                    ]]);
-        return response()->json(["mensaje" => "Comentario Registrado"], 201);
+            $museo = Museo::find($id);
+            $museo->comentarios()->attach([$user_id => [
+                "comentario" => $request->comentario,
+                "calificacion" => $request->calificacion,
+                "fecha" => date("Y-m-d H:i:s")
+            ]]);
+            return response()->json(["mensaje" => "Comentario Registrado"], 201);
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json(["mensaje" => "Error al realizar la petición", "error" => $e->getMessage()], 500);
+        }
     }
 }
